1f75df1626abec39bc1bee417d503fd500bbce75
William Chang <wilicw1346@gmail.com>
2022-10-22 12:34:15 +0800

    Recreate project with LL driver

---
+++ firmware/cmake-build-debug/CMakeFiles/firmware.elf.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_pwr.c.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/firmware.elf.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_pwr.c.obj: \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_pwr.c \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_ll_pwr.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/core_cm0plus.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_ll_bus.h
---
+++ firmware/cmake-build-debug/CMakeFiles/firmware.elf.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_usart.c.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/firmware.elf.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_usart.c.obj: \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_ll_usart.c \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_ll_usart.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/core_cm0plus.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_ll_rcc.h \
+ /Users/wilicw/Projects/MeiChu2022/firmware/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_ll_bus.h
