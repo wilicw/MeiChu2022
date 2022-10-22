7068c8670c36845901ac2d3353ecb331baae76ca
thomas <ddeng691@gmail.com>
2022-10-22 23:39:59 +0800

    update button state and layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import './App.css';
 import { Flex, Box, Text } from 'rebass';
-import { Button } from '@fluentui/react-components';
+import { ToggleButton } from '@fluentui/react-components';
 import DataVisualization from './components/DataVisualization';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
   const [humidity, setHumidity] = useState([{ time: 0, humidity: 0 }]);
   const [pressure, setPressure] = useState([{ time: 0, pressure: 0 }]);
   const [speed, setSpeed] = useState([{ time: 0, speed: 0 }]);
+  const [fanState, setFanState] = useState(0);
 
   const requestSerialPort = useCallback(() => {
     (navigator as any).serial.requestPort().then((port:any) => {
@@ -19,28 +20,37 @@ function App() {
         setInterval(() => {
           reader.read().then(({ value }) => {
             const v = value as string;
-            const splitted = v.split(',', 4);
-            if (splitted.length === 4) {
+            console.log(v);
+            const splitted = v.split(',', 6);
+            if (splitted.length === 6) {
+              // const t = parseInt(splitted[0], 10);
               setTemp((oldTemp) => [...oldTemp, {
                 time: oldTemp.slice(-1)[0].time + 1,
-                temp: parseInt(splitted[0], 10) / 10,
+                temp: parseInt(splitted[1], 10) / 10,
               }].slice(-10));
               setHumidity((oldHumidity) => [...oldHumidity, {
                 time: oldHumidity.slice(-1)[0].time + 1,
-                humidity: parseInt(splitted[1], 10) / 10,
+                humidity: parseInt(splitted[2], 10) / 10,
               }].slice(-10));
               setPressure((oldPressure) => [...oldPressure, {
                 time: oldPressure.slice(-1)[0].time + 1,
-                pressure: parseInt(splitted[2], 10) / 10,
+                pressure: parseInt(splitted[4], 10) / 10,
               }].slice(-10));
               setSpeed((oldSpeed) => [...oldSpeed, {
                 time: oldSpeed.slice(-1)[0].time + 1,
                 speed: parseInt(splitted[3], 10) / 10,
               }].slice(-10));
+              setFanState(() => parseInt(splitted[5], 10));
             }
+          }).catch((error: any) => {
+            alert(error);
           });
         }, 1000);
+      }).catch((error: any) => {
+        alert(error);
       });
+    }).catch((error: any) => {
+      alert(error);
     });
   }, []);
 
@@ -49,9 +59,14 @@ function App() {
       <Flex>
         <Text style={{ margin: 5 }} p={3} fontWeight="bold">Still Discussing...</Text>
         <Box mx="auto" />
-        <Button shape="rounded" style={{ margin: 10 }} appearance="primary" onClick={requestSerialPort}>
+        {fanState === 1 ? (
+          <ToggleButton shape="rounded" style={{ margin: 10 }} disabled>Fan Flipped</ToggleButton>
+        ) : (
+          <ToggleButton shape="rounded" style={{ margin: 10 }}>Fan Open</ToggleButton>
+        )}
+        <ToggleButton shape="rounded" style={{ margin: 10 }} appearance="primary" onClick={requestSerialPort}>
           Request Port
-        </Button>
+        </ToggleButton>
       </Flex>
       <Flex>
         <Box style={{ margin: 5 }} width={1 / 2} px={2}>
@@ -81,7 +96,7 @@ function App() {
             colors={['#28536b']}
             keys={['pressure']}
             chartName="Pressure"
-            unit="c"
+            unit="hPa"
             data={pressure}
             height={300}
           />
@@ -91,7 +106,7 @@ function App() {
             colors={['#28536b']}
             keys={['speed']}
             chartName="Wind Speed"
-            unit="c"
+            unit="m/s"
             data={speed}
             height={300}
           />
